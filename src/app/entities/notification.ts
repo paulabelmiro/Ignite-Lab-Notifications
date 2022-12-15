115e75d01b17bf3097c2c483402b98481008a9b9
Paula Belmiro <paulabelmiro@yahoo.com.br>
2022-12-15 16:59:24 -0300

    commit Jest Configuration and initial entities

---
+++ src/app/entities/notification.ts
@@ -0,0 +1,59 @@
+import { Replace } from "src/helpers/Replace";
+import { Content } from "./content";
+
+export interface NotificationProps {
+    recipienteId: string;
+    content: Content;
+    category: string;
+    readAt?: Date | null;
+    createdAt: Date;
+}
+
+export class Notification {
+    private props: NotificationProps;
+    
+    constructor(props: Replace<NotificationProps, {createdAt?: Date}>){
+        this.props = {
+            ...props,
+            createdAt: props.createdAt ?? new Date(),
+        }
+    }
+
+    public set recipienteId(recipienteId:string){
+        this.props.recipienteId = recipienteId;
+    }
+
+    public get recipienteId(): string {
+        return this.props.recipienteId;
+    }
+
+    public set content(content:Content){
+        this.props.content = content;
+    }
+
+    public get content(): Content {
+        return this.props.content;
+    }
+
+    public set category(category:string){
+        this.props.category = category;
+    }
+
+    public get category(): string {
+        return this.props.category;
+    }
+
+    public set readAt(readAt:Date | null | undefined){
+        this.props.readAt = readAt;
+    }
+
+    public get readAt(): Date | null | undefined {
+        return this.props.readAt;
+    }
+
+    public get createdAt(): Date {
+        return this.props.createdAt;
+    }
+}
+
+
