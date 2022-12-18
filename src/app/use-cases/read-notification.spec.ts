3db71b80878d60f32db16e012cb9f6f7d1a685a2
Paula Belmiro <paulabelmiro@yahoo.com.br>
2022-12-18 12:35:00 -0300

    create read and unread notification

--- src/app/use-cases/read-notification.spec.ts
+++ src/app/use-cases/read-notification.spec.ts
@@ -1,31 +1,31 @@
 import { makeNotification } from "@test/factories/notification-factory";
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
-import { CancelNotification } from "./cancel-notification";
+import { ReadNotification } from "./read-notification";
 
-describe('Cancel notification', () => {
+describe('Read notification', () => {
     it('should be able to read a notification', async () => {
         const notificationsRepository = new InMemoryNotificationsRepository();
-        const cancelNotification = new CancelNotification(notificationsRepository);
+        const readNotification = new ReadNotification(notificationsRepository);
 
         const notification = makeNotification();
 
         await notificationsRepository.create(notification);
 
-        await cancelNotification.execute({
+        await readNotification.execute({
             notificationId: notification.id,
         });
 
-        expect(notificationsRepository.notifications[0].cancelAt).toEqual(
+        expect(notificationsRepository.notifications[0].readAt).toEqual(
             expect.any(Date),
         );
     });   
     
     it('should not be able to read a non existing notification', async () => {
         const notificationsRepository = new InMemoryNotificationsRepository();
-        const cancelNotification = new CancelNotification(notificationsRepository);
+        const readNotification = new ReadNotification(notificationsRepository);
 
         expect(() => {
-            return cancelNotification.execute({
+            return readNotification.execute({
                 notificationId: 'fake-notification-id',
             });
         }).rejects.toThrow(Error);
