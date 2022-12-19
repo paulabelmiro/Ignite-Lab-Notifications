2ae05653e80b825261143780f6740dddbf8e4908
Paula Belmiro <paulabelmiro@yahoo.com.br>
2022-12-19 15:46:33 -0300

    update prisma service and roots

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -15,8 +15,10 @@ export class Notification {
     private _id: string;
     private props: NotificationProps;
     
-    constructor(props: Replace<NotificationProps, {createdAt?: Date}>){
-        this._id = randomUUID();
+    constructor(props: Replace<NotificationProps, {createdAt?: Date}>,
+        id?: string,
+        ){
+        this._id = id ?? randomUUID();
         this.props = {
             ...props,
             createdAt: props.createdAt ?? new Date(),
@@ -51,7 +53,7 @@ export class Notification {
         return this.props.category;
     }
 
-    public read(){
+    public read() {
         this.props.readAt = new Date();
     }
 
